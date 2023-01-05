1d9e76ce30073a5d00ae45a2151d5852d8f9ae03
Sayonara Almeida <sayow@hotmail.com>
2023-01-05 09:07:25 +0000

    Criacao do Schema

---
+++ db/migrate/20230105083550_create_properties.rb
@@ -0,0 +1,10 @@
+class CreateProperties < ActiveRecord::Migration[6.0]
+  def change
+    create_table :properties do |t|
+      t.string :name
+      t.string :units
+
+      t.timestamps
+    end
+  end
+end
