c51a2cd0a337f7abf1471513771b1fd11de36fad
Sayonara Almeida <sayow@hotmail.com>
2023-01-05 11:07:19 +0000

    Definicao de validacoes para o model

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,4 @@
 class Property < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :units, presence: true, allow_blank: false, inclusion: %w[kitchen bathroom bedroom living-room], :message=>"Please use one of the provided options for this property"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,4 @@ Rails.application.routes.draw do
       resources :properties
     end
   end
-
-
 end
