8d1774c8bf49f91476bd179ceef4bfd1c80cd6bf
Sayonara Almeida <sayow@hotmail.com>
2023-01-06 08:15:06 +0000

    Comecando a popular o seeds file

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,4 +2,6 @@ class Property < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :units, presence: true, allow_blank: false, inclusion: { in: %w[kitchen bathroom bedroom living-room],
                     message: 'Please use one of the provided options for this property'}
+  serialize :units, ::Array
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating 10 new properties...'
+10.times do
+  property = Property.create(
+    name:  "#{["Beautifu", "Lovely", "Bright", "Amazing", "Cozy", "Comfortable", "Modern", "Nice"].sample} #{["House", "Home", "Cottage", "Lodge", "Chateau", "Regency", "Villa", "Studio", "Bungalow", "Barn", "Loft"].sample}"
+    units: "#{}"
+  )
+  restaurant.save!
+end
+puts 'Finished!'
